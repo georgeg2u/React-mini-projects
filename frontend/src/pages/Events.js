8bc40efbc4bd78a1ca85667f19fdee588820fec0
George Gidoiu <george.gidoiu@gmail.com>
2023-08-17 19:12:28 +0300

    upload

---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,33 @@
+import {json, useLoaderData} from "react-router-dom";
+
+import EventsList from "../components/EventsList";
+
+function EventsPage() {
+  const data = useLoaderData();
+
+  //   if(data.isError) {
+  //       return <p>{data.message}</p>
+  //     }
+  const fetchedEvents = data.events;
+
+  return <EventsList events={fetchedEvents} />;
+}
+
+export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return {isError: true, message: 'Could not fetch events.'}
+    throw json(
+      {message: "Could not fetch events"},
+      {
+        status: 500,
+      }
+    );
+  } else {
+    const resData = await response.json();
+    return resData;
+  }
+}
---
+++ frontend/src/pages/NewEvent.js
@@ -0,0 +1,10 @@
+import EventForm from "../components/EventForm";
+
+const NewEventPage = () => {
+  return <EventForm  method="post"/>;
+};
+
+export default NewEventPage;
+
+
+
